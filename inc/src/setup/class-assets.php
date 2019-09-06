ce8506ea10d38ffa1603bcda5f1f0359e6e67268
Nicolas Huber <nicolasluishuber@gmail.com>
2019-09-06 09:33:12 -0300

    Revert "Allow WordPress serve for multiple domains"
    This reverts commit 559a716e813386b37f7d78ee7ac2dd56e6d4d56c.

--- inc/src/setup/class-assets.php
+++ inc/src/setup/class-assets.php
@@ -15,13 +15,6 @@ use DI\Container;
  */
 class Assets extends Base {
 	/**
-	 * Current version
-	 *
-	 * @var string
-	 */
-	const VERSION = '0.1';
-
-	/**
 	 * Add hooks
 	 */
 	public function init() {
@@ -43,15 +36,23 @@ class Assets extends Base {
 	 * Load application style
 	 */
 	public function enqueue_styles() {
-		wp_enqueue_style( 'aztec-env', $this->assets_uri( 'app.css' ), [], self::VERSION );
+		wp_enqueue_style( 'aztec-env', $this->assets_uri( 'app.css' ), array(), self::VERSION );
 	}
 
 	/**
 	 * Load application script
 	 */
 	public function enqueue_script() {
-		wp_enqueue_script( 'aztec-env-vendor', $this->assets_uri( 'vendor.js' ), [ 'jquery' ], self::VERSION, true );
-		wp_enqueue_script( 'aztec-env-app', $this->assets_uri( 'app.js' ), [], self::VERSION, true );
-		wp_localize_script( 'aztec-env-app', 'aztec_env', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+		wp_enqueue_script( 'aztec-env-vendor', $this->assets_uri( 'vendor.js' ), array( 'jquery' ), self::VERSION, true );
+		wp_enqueue_script( 'aztec-env-app', $this->assets_uri( 'app.js' ), array(), self::VERSION, true );
+
+		// Append PHP data to JS.
+		wp_localize_script(
+			'aztec-env-app',
+			'aztec_env',
+			array(
+				'ajax_url' => admin_url( 'admin-ajax.php' ),
+			)
+		);
 	}
 }
