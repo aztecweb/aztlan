d64fae203126e01959cf665324a1b44a34c36066
Mateus <mateus@aztecweb.net>
2020-11-05 13:44:21 -0300

    Substituido remoçao do diretorio build e build_clean_files por um comando Docker

---
+++ environment/bin/shared/docker_utils.sh
@@ -0,0 +1,11 @@
+#######################################
+# Run cleanup directories using a Docker container
+# structure
+# Arguments:
+#   The directory to remove
+# Returns:
+#   None
+#######################################
+cleanup() {
+	docker run -it -v ${PROJECT_ROOT_PATH}:/app -w /app alpine sh -c "rm -rf $@"
+}
