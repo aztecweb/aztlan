a57bacdd0e376d3cf27d90e389889069d86c7d82
Mateus Roveda <mateus@aztecweb.net>
2021-02-17 20:21:10 +0000

    Resolve "Implementar testes de aceitação"

---
+++ phpqa/tests/acceptance/HomePageCest.php
@@ -0,0 +1,15 @@
+<?php
+
+class HomePageCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Welcome to Aztlan');
+    }
+}
