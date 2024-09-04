df1339f00dc7391fb6e42e8195a51a2295bc618a
Marcos <marcos@aztecweb.net>
2024-09-04 17:11:53 -0300

    Merge branch '194-atualizar-versoes-de-pacotes' into 'staging'

--- inc/src/aztlan/log/class-log.php
+++ inc/src/aztlan/log/class-log.php
@@ -20,63 +20,64 @@ class Log implements LoggerInterface {
 	/**
 	 * {@inheritDoc}
 	 */
-	public function emergency( $message, array $context = array() ): void {
+	public function emergency( string|\Stringable $message, array $context = array() ): void {
 		$this->logger->emergency( $message, $context );
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public function alert( $message, array $context = array() ): void {
+	public function alert( string|\Stringable $message, array $context = array() ): void {
 		$this->logger->alert( $message, $context );
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public function critical( $message, array $context = array() ): void {
+	public function critical( string|\Stringable $message, array $context = array() ): void {
 		$this->logger->critical( $message, $context );
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public function error( $message, array $context = array() ): void {
+	public function error( string|\Stringable $message, array $context = array() ): void {
 		$this->logger->error( $message, $context );
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public function warning( $message, array $context = array() ): void {
+	public function warning( string|\Stringable $message, array $context = array() ): void {
 		$this->logger->warning( $message, $context );
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public function notice( $message, array $context = array() ): void {
+	public function notice( string|\Stringable $message, array $context = array() ): void {
 		$this->logger->notice( $message, $context );
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public function info( $message, array $context = array() ): void {
+	public function info( string|\Stringable $message, array $context = array() ): void {
 		$this->logger->info( $message, $context );
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public function debug( $message, array $context = array() ): void {
+	public function debug( string|\Stringable $message, array $context = array() ): void {
 		$this->logger->debug( $message, $context );
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public function log( $level, $message, array $context = array() ): void {
+	// @phpstan-ignore-next-line: $level has no type specified to be compatible with Psr\Log\LoggerInterface::log.
+	public function log( $level, string|\Stringable $message, array $context = array() ): void {
 		$this->logger->log( $level, $message, $context );
 	}
 }
